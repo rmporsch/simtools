138730b88010765e4b4a7560cb54a7bda9d1eaf0
Robert Porsch <rmporsch@gmail.com>
2018-03-06 13:44:05 +0800

    Added a better way to load a genotype matrix, needs some more cleanup later

--- simtools/genotypes.py
+++ simtools/genotypes.py
@@ -7,7 +7,8 @@ import vcf
 import os
 from pyplink import PyPlink
 
-def simple_genotype_matrix(n, p, min_maf=0.05, max_maf=0.5):
+
+def simple_genotype_matrix(n, p):
     """Generates a simple matrix containing either 0 or 1 of size nxp
 
     :n: number of samples
@@ -19,7 +20,7 @@ def simple_genotype_matrix(n, p, min_maf=0.05, max_maf=0.5):
     """
     genotypes = np.zeros(shape=(n, p))
     for item in range(0, p):
-        genotypes[:,item] = np.random.binomial(1, np.random.uniform(0.1, 0.5, 1), n)
+        genotypes[:, item] = np.random.binomial(1, np.random.uniform(0.1, 0.5, 1), n)
 
     return genotypes
 
@@ -33,32 +34,96 @@ class ReadVCF(object):
         :vcffile: path to the vcf file
 
         """
-        self._vcffile = vcffile
-        
-        
-    def read_vcf(self, index_file=False):
+        self._vcf_file = vcffile
+        self._samples = None
+        self._randomset = None
+        self._get_samples()
+
+    def _get_samples(self):
+        reader = vcf.Reader(filename=self._vcf_file)
+        self._samples = reader.samples
+
+    def _get_genotypes(self, samples, records, switch):
+        variant = np.zeros(len(samples))
+        for idx, sample in enumerate(samples):
+            try:
+                gt = records.genotype(sample)['GT']
+            except IndexError:
+                print("something went wrong with:")
+                print('sample:', sample, 'variant:', records, '-- set value to missing')
+                gt = '.'
+            if gt == '.':
+                gt = 0
+            else:
+                gt = re.split('\||/', gt)
+                gt = list(map(int, gt))
+            variant[idx] = np.sum(gt)
+        if switch:
+            variant = np.abs(variant - 2)
+        return variant
+
+    def criteria(self, record):
+        process = True
+        switch = False
+        if record.call_rate <= 0.9:
+            process = False
+        if not record.is_snp:
+            process = False
+        if len(record.aaf) > 1:
+            process = False
+
+        if record.aaf[0] > 0.5:
+            if (1-record.aaf[0]) > 0.01:
+                process = False
+            switch = True
+        else:
+            if record.aaf[0] > 0.01:
+                process = False
+        return process, switch
+
+    def _check_samples(self, samples):
+        check = [k not in self._samples for k in samples]
+        num_not_in_vcf = np.sum(check)
+        if num_not_in_vcf > 0:
+            print(num_not_in_vcf, 'were not in vcf file and were removed')
+        return samples[~np.array(check)]
+
+    def load_genotype_matrix(self, subjects, variants):
+        if not os.path.isfile(variants):
+            raise NameError('File does not exist')
+        subjects = self._check_samples(subjects)
+        variants = pd.read_table(variants, header=None, sep=' ')
+        if variants.shape[1] < 2:
+            raise NameError('Variant file does not seem to have the right amount of columns')
+        matrix = np.zeros((len(subjects), variants.shape[0]))
+        skiped_variants = []
+
+        vcf_reader = vcf.Reader(filename=self._vcf_file)
+        for index, row in variants.iterrows():
+            record = vcf_reader.fetch(str(row[0]), row[1], row[1])
+            process, switch = self.criteria(record)
+            if not process:
+                skiped_variants.append(index)
+                continue
+            matrix[:, index] = self._get_genotypes(samples, record, switch)
+
+        # remove skiped variants
+        matrix = np.delete(matrix, skiped_variants, 1)
+        return matrix
+
+    def get_allele_freq(self, output_path):
         """Reads the vcf file and gets and index
 
         :index_file: location of the index file
 
         """
-        vcf_reader = vcf.Reader(filename=self._vcffile)
-        self._samples = vcf_reader.samples
+        vcf_reader = vcf.Reader(filename=self._vcf_file)
+        with open(output_path, 'w') as f:
+            for record in vcf_reader:
+                f.write('%s %i %s %f\n' %
+                        (record.CHROM, record.POS, record.ID, record.aaf))
 
-        temp_folder = '.temp'
-        temp_file = '.temp/variant.list'
-
-        if not os.path.exists(temp_folder):
-            os.makedirs(temp_folder)
-
-        if not os.path.isfile(temp_file) or index_file:
-            with open(temp_file, 'w') as f:
-                for record in vcf_reader:
-                    f.write('%s %i %s %f\n' %
-                            (record.CHROM, record.POS, record.ID, record.INFO['AF']))
-
-
-    def __sample_variants(self, maf, p, write_disk=False):
+    def __sample_variants(self, p, write_disk=False):
         """Sample a random set of variants
 
         :maf: minor allele frequency cutoff
@@ -72,7 +137,6 @@ class ReadVCF(object):
         self._variants = self._variants.query('AF >= @maf and AF <= (1- @maf)')
         self._sampled = self._variants.sample(n=p)
         
-
         if write_disk:
             self._sampled.to_csv('.temp/subsample_variant.list')
 
@@ -84,7 +148,7 @@ class ReadVCF(object):
         """
         self._randomset = np.random.choice(self._samples, n, replace=True)
 
-    def sample(self, n, p, maf=0.05, write_disk=False):
+    def sample(self, n, p, write_disk=False):
         """Random sample a set of variants and subjects
 
         :maf: minor allele frequency cutoff
@@ -94,16 +158,16 @@ class ReadVCF(object):
         :returns: a numpy matrix of size n*p
 
         """
-        self.__sample_variants(maf, p, write_disk)
+        self.__sample_variants(p, write_disk)
         self.__sample_subjects(n)
 
         genotypematrix = np.zeros((n,p))
-        reader = vcf.Reader(filename=self._vcffile)
+        reader = vcf.Reader(filename=self._vcf_file)
         i = 0
         for index, row in self._sampled.iterrows():
-            record = reader.fetch(str(row[0]), row[1]-1, row[1])
+            record = reader.fetch(str(row[0]), row[1], row[1])
             for rr in record:
-                flip = True if rr.INFO['AF'] > 0.5 else False
+                flip = True if rr.aaf > 0.5 else False
                 u = 0
                 for sample in rr.samples:
                     if sample.sample in self._randomset:
@@ -125,7 +189,6 @@ class ReadVCF(object):
         if flip:
             genotypes = abs(genotype - 1)
         return sum(genotype)
-
 
 
 class ReadPlink(object):
