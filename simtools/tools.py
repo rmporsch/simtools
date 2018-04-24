d578cd0a0968a29e13b79b05018706e57f6b3096
Robert Porsch <rmporsch@gmail.com>
2018-04-24 17:49:23 +0800

    tools.py is now PEP8 comform

--- simtools/tools.py
+++ simtools/tools.py
@@ -31,10 +31,11 @@ def qqplot(dat, grouping='pheno', pvalue='pvalue'):
             temp = group[['pvalue']]
             n = temp.shape[0]
             temp = temp.sort_values(pvalue)
-            temp['null'] = np.arange(1, n+1)/ n
+            temp['null'] = np.arange(1, n+1)/n
             temp = temp.apply(lambda x: -1*np.log(x))
-            plt.plot(temp.null, temp.pvalue,'.', label=str(name))
+            plt.plot(temp.null, temp.pvalue, '.', label=str(name))
             plt.plot(temp.null, temp.null)
+            plt.
         plt.legend(loc='upper left')
 
     plt.xlabel('Expected')
@@ -53,11 +54,11 @@ def inflation_factor(s, stats_type='pval', rounding=3):
 
     """
     z = s
-    if stats_type=='pval':
+    if stats_type == 'pval':
         z = scipy.stats.norm.ppf(s/2)
-    if stats_type=='chisq':
+    if stats_type == 'chisq':
         z = np.sqrt(s)
-    if stats_type=='z':
+    if stats_type == 'z':
         z = s
 
     lamb = np.round(np.median(z**2)/0.454, rounding)
@@ -74,11 +75,11 @@ def causal_models(causal_id, num_pheno):
     """
     lamb = np.zeros((num_pheno, num_pheno))
     if causal_id == 1:
-        num_edges = num_pheno -1
+        num_edges = num_pheno - 1
         arr = np.arange(num_pheno)
         np.random.shuffle(arr)
         i = 0
-        while i <num_edges:
+        while i < num_edges:
             lamb[arr[i+1], arr[i]] = 1
             i += 1
         return lamb
@@ -94,9 +95,9 @@ def causal_models(causal_id, num_pheno):
         num_pairs = 1
         arr = np.arange(num_pheno)
         np.random.shuffle(arr)
-        if num_pheno % 2 !=0:
-            arr = np.delete(arr,0)
-        edges = np.split(arr, len(arr)/2) 
+        if num_pheno % 2 != 0:
+            arr = np.delete(arr, 0)
+        edges = np.split(arr, len(arr)/2)
         for i in edges:
             lamb[i[0], i[1]] = 1
         return lamb
@@ -106,10 +107,11 @@ def causal_models(causal_id, num_pheno):
         arr = np.append(arr, arr[0])
         num_edges = num_pheno
         i = 0
-        while i <num_edges:
+        while i < num_edges:
             lamb[arr[i+1], arr[i]] = 1
             i += 1
         return lamb
+
 
 def causal_model_adjustment(lamb):
     """Evaluates an adjacency matrix and adjust it if necessary
@@ -124,13 +126,14 @@ def causal_model_adjustment(lamb):
     # identify if causal re-direction is required
     rowsum = np.sum(lamb, axis=1)
     colsum = np.sum(lamb, axis=0)
-    if ((np.any(rowsum > 1) & np.any(colsum >1)) | np.sum(colsum == 0) > 1):
+    if ((np.any(rowsum > 1) & np.any(colsum > 1)) | np.sum(colsum == 0) > 1):
         return lamb
     else:
-        start = np.where(rowsum==0)[0]
-        end = np.where(colsum==0)[0]
+        start = np.where(rowsum == 0)[0]
+        end = np.where(colsum == 0)[0]
         lamb[end, start] = 1.0
-        return lamb 
+        return lamb
+
 
 class Plink(object):
 
@@ -145,10 +148,10 @@ class Plink(object):
         """
         self._plink_stem = plink_stem
 
-        if plink_path=='auto':
+        if plink_path == 'auto':
             self._bin_plink = '/usr/bin/plink'
         else:
-            self._bin_plink=plink_path
+            self._bin_plink = plink_path
 
         self._plink = ReadPlink(self._plink_stem)
 
@@ -164,15 +167,16 @@ class Plink(object):
         # write input files for plink
         pheno = pd.DataFrame()
 
-        if  phenotype.ndim > 1:
-            pheno = pd.DataFrame(phenotype,
+        if phenotype.ndim > 1:
+            pheno = pd.DataFrame(
+                    phenotype,
                     columns=['p'+str(k+1) for k in range(phenotype.shape[1])])
         else:
             pheno = pd.DataFrame(phenotype, columns=['pheno'])
         if subjects is None:
             subjects = self._plink.fam.iid.values
 
-        fam = pd.DataFrame({'fid':subjects, 'iid':subjects})
+        fam = pd.DataFrame({'fid': subjects, 'iid': subjects})
         pheno = pd.concat([fam, pheno], axis=1)
         pheno.to_csv('/tmp/temp_pheno', index=False, sep=' ')
 
@@ -182,12 +186,15 @@ class Plink(object):
 
         output_location = '/tmp/plink_temp'
         with open(os.devnull, 'w') as fp:
-            plink_run = Popen([self._bin_plink,'--bfile', self._plink_stem,
-                    "--allow-no-sex",
-                    '--pheno', '/tmp/temp_pheno',
-                    '--all-pheno', 
-                    "--linear", model_paramters,
-                    '--out', output_location], stdout=fp)
+            plink_run = Popen([
+                                self._bin_plink,
+                                '--bfile', self._plink_stem,
+                                "--allow-no-sex",
+                                '--pheno', '/tmp/temp_pheno',
+                                '--all-pheno',
+                                "--linear", model_paramters,
+                                '--out', output_location
+                                ], stdout=fp)
             plink_run.wait()
 
         assoc_files = glob.glob('/tmp/plink_temp.*.assoc.*')
@@ -196,9 +203,10 @@ class Plink(object):
             ValueError('Plink failed to generate any resutls')
         elif len(assoc_files) > 1:
             out_results = []
+            imported_file_expression = \
+                r"(/tmp/plink_temp\.|\.assoc\.|\.linear|\.logistic)"
             for f in assoc_files:
-                pheno_name = re.sub(r"(/tmp/plink_temp\.|\.assoc\.|\.linear|\.logistic)",
-                        '', f)
+                pheno_name = re.sub(imported_file_expression, '', f)
                 print(pheno_name)
                 temp = pd.read_table(f, delim_whitespace=True)
                 temp['Phenotype'] = pheno_name
@@ -208,7 +216,11 @@ class Plink(object):
             results = pd.read_table(assoc_files[0], delim_whitespace=True)
 
         if results.shape[0] < 1:
-            ValueError('Plink failed to generate any resutls or result were not imported')
+            ValueError(
+                """
+                Plink failed to generate any results or failed import
+                """
+            )
 
         return results
 
@@ -221,7 +233,9 @@ class Plink(object):
 
         """
         if not isinstance(weights, pd.DataFrame):
-            ValueError('weights must be a pandas dataframe with rsid, allele and weight')
+            ValueError(
+                'weights must be a pandas frame with rsid, allele and weight'
+                )
 
         output_location = '/tmp/plink_prs'
 
@@ -233,21 +247,25 @@ class Plink(object):
         if subjects is None:
             self._plink.fam.to_csv(subjectfile, index=False, sep=' ')
         else:
-            pd.DataFrame({'IID': subjects,
-                'FID': subjects}).to_csv(subjectfile, index=False, sep=' ')
+            pd.DataFrame({
+                    'IID': subjects,
+                    'FID': subjects}).to_csv(subjectfile, index=False, sep=' ')
 
         # write score file
         scorefile = '/tmp/plink_prs_weights.score'
         weights.to_csv(scorefile, index=False, sep=' ', header=False)
 
         with open(os.devnull, 'w') as fp:
-            plink_run = Popen([self._bin_plink, '--bfile', self._plink_stem,
+            plink_run = Popen([
+                self._bin_plink, '--bfile', self._plink_stem,
                 '--allow-no-sex', '--score', scorefile,
-                '--keep', subjectfile, '--out', output_location], stdout=fp)
+                '--keep', subjectfile, '--out', output_location],
+                 stdout=fp)
 
             plink_run.wait()
 
-        results = pd.read_table(output_location+'.profile', delim_whitespace=True)
+        results = pd.read_table(
+            output_location+'.profile', delim_whitespace=True)
         return results
 
     def clumping(self, summary_stats, p1=0.0001, p2=0.01, r2=0.5, kb=250):
@@ -269,22 +287,23 @@ class Plink(object):
         var_names = summary_stats.columns
         summary_stats.to_csv(output_location+'.report', index=False, sep=' ')
 
-
         with open(os.devnull, 'w') as fp:
-            plink_run = Popen([self._bin_plink, '--bfile', self._plink_stem,
+            plink_run = Popen([
+                self._bin_plink, '--bfile', self._plink_stem,
                 '--allow-no-sex', '--clump', output_location+'.report',
                 '--clump-p1', str(p1), '--clump-p2', str(p2),
-                '--clump-r2', str(r2), 
-                '--clump-kb', str(kb), 
+                '--clump-r2', str(r2),
+                '--clump-kb', str(kb),
                 '--out', output_location], stdout=fp)
 
             plink_run.wait()
 
-        results = pd.read_table(output_location+'.clumped', delim_whitespace=True)
+        results = pd.read_table(
+            output_location+'.clumped', delim_whitespace=True)
         return results
 
     def pruning(self, kp, step, r2):
-        """Performs variant pruning and returns a set of variants in linkage 
+        """Performs variant pruning and returns a set of variants in linkage
         equilibrium
 
         :kp: window size
@@ -300,12 +319,14 @@ class Plink(object):
                 os.remove(filename)
 
         with open(os.devnull, 'w') as fp:
-            plink_run = Popen([self._bin_plink, '--bfile', self._plink_stem,
+            plink_run = Popen([
+                self._bin_plink, '--bfile', self._plink_stem,
                 '--allow-no-sex',
                 '--indep-pairwise', kp+' '+step+' '+r2,
                 '--out', output_location], stdout=fp)
 
             plink_run.wait()
 
-        results = pd.read_table(output_location+'.prune.in', delim_whitespace=True)
+        results = pd.read_table(
+            output_location+'.prune.in', delim_whitespace=True)
         return results
